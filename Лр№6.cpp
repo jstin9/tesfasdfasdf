6df3ec3bfd75eed468bc8cc89c1115ee5d2681fd
BuildTools <unconfigured@null.spigotmc.org>
2023-11-07 22:47:18 +0200

    firts commitgit add .!

---
+++ Лр№6.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include "windows.h"
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    
+    int a, b; // a - початкове значення x; b - кінцеве значення x
+    double h; // h - крок за x
+    cout << "Введіть a: ";
+    cin >> a;
+    cout << "Введіть b: ";
+    cin >> b;
+    cout << "Введіть h: ";
+    cin >> h;
+
+    double y, x = a;
+    while (x <= b) {
+        y = pow(x, 8) + 5 * pow(x, 2) - 5;
+        cout << setw(10) << "x=" << fixed << setprecision(1) << x << setw(10) << "y=" << y << endl;
+        x += h;
+    }
+    do {
+        y = pow(x, 8) + 5 * pow(x, 2) - 5;
+        cout << setw(10) << "x=" << fixed << setprecision(1) << x << setw(10) << "y=" << y << endl;
+        x += h;
+    } while (x <= b);
+}
+
+
